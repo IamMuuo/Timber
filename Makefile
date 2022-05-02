af33c879f4b911fa6f347456abb670eb57af8f7a
erick <hearteric57@gmail.com>
2022-05-02 19:16:09 +0300

    Added working background

--- Makefile
+++ Makefile
@@ -2,3 +2,13 @@
 #	Makefile For project timber
 #	Author:	Eric Muuo
 #################################################
+
+c++ = g++	#use the gnu compiler
+libs = -lsfml-graphics -lsfml-window -lsfml-system
+flags = -g -Wall -Wextra -std=c++17
+
+timber: main.o
+	$(c++) main.o -o timber $(libs)
+
+main.o: main.cpp
+	$(c++) main.cpp -c -o main.o $(flags)
--- main.cpp
+++ main.cpp
@@ -7,7 +7,61 @@
 *   Emulates the clasic timber game
 *******************************************************/
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/WindowStyle.hpp>
+
 int main()
 {
-    // write code here...
+    // create a video mode object
+    sf::VideoMode vm(1920,1080);
+
+    // create and open a window for the game
+    sf::RenderWindow window(vm,"Timber",sf::Style::Fullscreen);
+
+    // Texture to hold graphic onto the GPU
+    sf::Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+
+    // sprite to draw into the screen
+    sf::Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+
+    // set the background to cover the screen
+    spriteBackground.setPosition(0,0);
+
+    while (window.isOpen()) // game main loop
+    {   
+        /************************************************
+        *   Handle the player input
+        ************************************************/
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            window.close(); // close the window
+        }
+
+
+        /***********************************************
+        *   Update the game scenes
+        ***********************************************/
+
+
+        /***********************************************
+        *   Draw the game scenes
+        ***********************************************/
+
+        // clear everything from the last frame
+        window.clear();
+
+        // draw thw game scenes
+        window.draw(spriteBackground);
+
+        // show everything we drew
+        window.display();
+    }
+    return 0;    
 }
